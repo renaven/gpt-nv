7154c4f444b114613e84f82b031de2e01e1c8ebf
Eric Maxwell <your.emperor@gmail.com>
2023-07-11 20:02:41 +0300

    basic

--- gpp-test.py
+++ gpp-test.py
@@ -1,17 +1,28 @@
-import openai
+from nv import nv_grab, gpt_query
+from time import sleep
 
-openai.api_key = "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"
+with open('in.txt') as file:
+    lines = [line.rstrip() for line in file]
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a chatbot"},
-        {"role": "user", "content": "Can you edit a news article for clarity and readbility, confroming with AP style gyuide?"},
-    ]
-)
+count = 1
+for url in lines:
+    print(url)
+    article = nv_grab(url)
+    query = article['head']
 
-result = ''
-for choice in response.choices:
-    result += choice.message.content
+    for p in article['body']:
+        query = query + '\n' + p
 
-print(result)
+    prefix = "I need you to translate a news article from Ukrainian into English, and then edit it in accordance with the AP style gyuide, prioritizing clarity and readability. Here is the article, beggining with the title: "
+
+    result = gpt_query(prefix, query)
+
+    with open(str(count)+'.txt', 'w', encoding='utf-8') as f:
+        for l in result:
+            f.write(l)
+        f.write('\n\n' + url + '\n\n')
+        f.write(article['head'] + '\n')
+        for p in article['body']:
+            f.write(p + '\n')
+    count = count + 1
+    sleep(4)
